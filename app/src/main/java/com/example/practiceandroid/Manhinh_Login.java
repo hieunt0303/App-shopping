d9007d076d3ea2aa26a0ddbedf7b0238b42b6a2e
Gnol0909 <sieutromlong1412@gmail.com>
2021-04-13 20:56:53 +0700

    Login

---
+++ app/src/main/java/com/example/practiceandroid/Manhinh_Login.java
@@ -0,0 +1,22 @@
+package com.example.practiceandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.os.Bundle;
+
+public class Manhinh_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manhinh__login);
+    }
+}
