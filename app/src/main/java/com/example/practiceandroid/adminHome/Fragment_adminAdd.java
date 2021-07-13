5a9f432f5ef3919bcaf8b1ca1e2cbbd6dc236a42
hieunt0303 <bibi030301@gmail.com>
2021-07-13 09:58:47 +0700

    haha

--- app/src/main/java/com/example/practiceandroid/adminHome/Fragment_adminAdd.java
+++ app/src/main/java/com/example/practiceandroid/adminHome/Fragment_adminAdd.java
@@ -61,6 +61,6 @@ public class Fragment_adminAdd extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_add, container, false);
+        return inflater.inflate(R.layout.activity_addproduct, container, false);
     }
 }
